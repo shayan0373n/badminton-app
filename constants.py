ee6360c21ff63ebf6588c9512b141b23434fd2ce
Shayan Taheri <airplanetaheri@gmail.com>
2025-07-31 13:10:40 +0300

    Added a small penalty for matching FF teams against non FF teams.

---
+++ constants.py
@@ -0,0 +1,6 @@
+# Utils Constants
+PLAYERS_PER_COURT = 4 # Although this is defined as a constant, only 4 players per court is currently supported.
+
+# Setup Constants
+DEFAULT_NUM_COURTS = 2
+DEFAULT_WEIGHTS = {'skill': 1, 'power': 3, 'pairing': 2, 'ff_power_penalty': -1}
